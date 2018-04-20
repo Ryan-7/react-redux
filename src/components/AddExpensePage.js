06479023a8e7a156cbaa3b852e715d302e28e7a9
Burch, Ryan <rburch@silverstarbrands.com>
2018-04-20 09:26:41 -0500

    working

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { addExpense } from '../actions/expenses';
 
-const AddExpensePage = () => (
-    <p>Add Expense Component</p>
+const AddExpensePage = (props) => (
+    <div>
+        <p>Add Expense Component</p>
+        <form  onSubmit={(e) => {
+            e.preventDefault();
+            let description = e.target.elements.expense.value;
+            props.dispatch(addExpense({description}));
+        }}>
+        <input type="text" placeholder="Item description"  name="expense" />
+        <button>Add expense</button>
+        </form>
+    </div>
 )
 
-export default AddExpensePage;
+export default connect()(AddExpensePage);
