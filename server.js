aa5320aa94c3b6e3e0e320738128951de215c1e5
Burch, Ryan <rburch@silverstarbrands.com>
2018-05-03 08:39:59 -0500

    heroku setup

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ app.get('*', (req, res) => {
    res.sendFile(__dirname + '/public/index.html')
 })
 
-app.listen(3000, () => {
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
     console.log(`Server running on port 3000`);
 })
